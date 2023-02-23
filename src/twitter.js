0b17569e4b5a0121cec30537d4805f596ac9aa5a
surajbca <krsurajkumar7481@gmail.com>
2023-02-23 18:43:25 +0530

    fake twitter

--- src/twitter.js
+++ src/twitter.js
@@ -36,3 +36,68 @@ function preview_image(event, id) {
     reader2.readAsDataURL(event.target.files[0]);
   }
 }
+
+/* from submit*/
+function updateInput(value) {
+  /*document.getElementById("").innerHTML =
+    document.getElementById("input1").value;
+
+  document.getElementById("").innerHTML =
+    document.getElementById("input2").value;
+
+ */
+
+  document.getElementById("time").innerHTML =
+    document.getElementById("input3").value;
+
+  document.getElementById("Battery").innerHTML =
+    document.getElementById("input4").value;
+
+  document.getElementById("name").innerHTML =
+    document.getElementById("input5").value;
+
+  document.getElementById("userName").innerHTML =
+    document.getElementById("input6").value;
+
+  document.getElementById("Born").innerHTML =
+    document.getElementById("input7").value;
+
+  document.getElementById("join").innerHTML =
+    document.getElementById("input8").value;
+
+  document.getElementById("following").innerHTML =
+    document.getElementById("input9").value;
+
+  document.getElementById("followers").innerHTML =
+    document.getElementById("input10").value;
+}
+/*unction formSubmit() {
+  document.getElementById("FormID").submit();
+}*/
+
+/*mobile display */
+function mobileStyle() {
+  var element = document.getElementById("myElement");
+  element.style.margin = "23px 135px 24px 141px";
+}
+
+function desktopStyle() {
+  var element = document.getElementById("myElement");
+  element.style.margin = "0px";
+}
+
+/*button style*/
+/*function submitButtonStyle() {
+  document.getElementsByClassName("is-fullwidth").style.backgroundColor = "red";
+}*/
+
+/*document.getElementById("smit").onclick = function () {
+  var checkedPosition = document.querySelectorAll(
+    "#player_stuff [name='position']:checked"
+  );
+  var playerId = document.querySelector("#player_stuff [name='player_id']");
+  var position = checkedPosition[0].value;
+  var player_id = playerId.value;
+
+  document.getElementById("disp_player").innerHTML = position + " " + player_id;
+}; */
